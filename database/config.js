9c4cfd93b37a181050858a98d04511618f6ed2ed
bapasidik13-afk <bapasidik13@gmail.com>
2025-12-19 13:24:05 +0700

    Add database/config.js

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8429589386:AAH3pHX89ZDxF9As5BAgygbv0JdicBzs_l4",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7546117130", // Ubah Jadi Id Mu !!!
+  port: "2406", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "188.166.176.226" // Ubah Jadi Ip Vps Mu !!!
+};
